8f60d99ca5e47bd8ee47d30c57fcc61e31edf123
Chris Barber-Riley <chris.barberriley@gmail.com>
2020-08-16 21:31:05 +0100

    Simple routing

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
+const api = require("./api");
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello world");
 });
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+app.use("/api/v1", api);
