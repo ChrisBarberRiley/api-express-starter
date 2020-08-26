3e6e5df451ccd0c10416af87084dfa57c8c1289d
Chris Barber-Riley <chris.barberriley@gmail.com>
2020-08-26 15:46:28 +0100

    Added controlled routes

--- src/api/users.js
+++ src/api/users.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const { getUsers, getUser, createUser } = require("../controllers/users");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
+// Standard routing
+router.get("/all-users", (req, res) => {
     res.json({
-        message: "Users route",
+        message: "Users route!",
     });
 });
+
+// Controller routes
+router.route("/").get(getUsers);
+router.route("/user/:id").get(getUser).post(createUser);
 
 module.exports = router;
